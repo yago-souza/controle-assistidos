117440e1adcee75f2a91fbb909758ceedc49e5df
Yago <yagos-s@live.com>
2023-06-28 16:31:38 -0300

    Separando Controller da View, trabalhando com o blade e criando view, controler e rota do create. E criando um layout

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -14,16 +14,19 @@ class SeriesController extends Controller
             'Grey\'s Anatomy'
         ];
 
-        $html = '<ul>';
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
-
         #return $request->get('id');
         #return $request->query('id');
         #return redirect('https://google.com.br');
         #return response('', 301, ['Location' => 'https://google.com.br']);
-        return $html;
+        #return $html;
+        /*return view(
+            'listar-series', ['series' => $series]
+        ); a função compact retorna o mesmo resultado que ['series' => $series]*/
+        #return view('listar-series', compact('series'));
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create() {
+        return view('series.create');
     }
 }
---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,8 @@
+<x-layout title="Séries">
+    <a href="/series/criar">Adicionar</a>
+ <ul>
+     @foreach ($series as $serie)
+     <li>{{ $serie }}</li>
+     @endforeach
+ </ul>
+</x-layout>
